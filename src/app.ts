1ffe1dd908c6462b7f515ec9ca6fe11f12a7fa73
Sakil71 <shakilahamed2171@gmail.com>
2025-07-04 06:40:58 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import bookRoutes from "./app/routes/bookRoutes";
 import borrowRoutes from "./app/routes/borrowRoutes";
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/books", bookRoutes);
 app.use("/api/borrow", borrowRoutes);
